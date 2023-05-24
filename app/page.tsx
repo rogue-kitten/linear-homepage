9ca30fd146a16e6de5851ab37e4c13b6df99dce2
Nash <nashbach69@gmail.com>
2023-05-24 22:42:20 +0530

    adds the sunrise gradient

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import classNames from 'classnames';
 import { Button } from './components/button';
 import { Clients } from './components/clients';
 import { Container } from './components/container';
 import { Hero, HeroSubtitle, HeroTitle } from './components/hero';
 import { HeroImage } from './components/heroImage';
 import { ChevronRight } from './components/icons/chevronRight';
+import { Stars } from './components/icons/stars';
 
 export default function Home() {
   return (
@@ -47,6 +49,15 @@ export default function Home() {
       <Container className='mt-[12.8rem] text-center md:mt-[25.6rem]'>
         <Clients />
       </Container>
+      <div
+        className={classNames(
+          'sunrise-mask bg-sunrise-background relative mx-auto my-[-12.8rem] h-[60rem] overflow-hidden',
+          '[--color:#7877C6] before:absolute before:inset-0 before:h-full before:w-full before:bg-sunrise-gradient before:opacity-40',
+          'after:absolute after:-left-1/2 after:top-1/2 after:aspect-1/0.7 after:w-[200%] after:rounded-[50%] after:border-t after:border-[#7876c566] after:bg-background'
+        )}
+      >
+        <Stars />
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -52,12 +52,16 @@ module.exports = {
       12: '4.8rem',
       16: '6.4rem'
     },
+    aspectRatio: {
+      '1/0.7': '1 / 0.7'
+    },
     backgroundImage: {
       "primary-gradient": 'linear-gradient(92.88deg, rgb(69, 94, 181) 9.16%, rgb(86, 67, 204) 43.89%, rgb(103, 63, 215) 64.72%)',
       "page-gradient": "radial-gradient(ellipse 80% 50% at 50% -20%,rgba(120,119,198,0.3),transparent);",
       "hero-gradient": "radial-gradient(ellipse 50% 80% at 20% 40%,rgba(93,52,221,0.1),transparent), radial-gradient(ellipse 50% 80% at 80% 50%,rgba(120,119,198,0.15),transparent);",
       "hero-glow": "conic-gradient(from 230.29deg at 51.63% 52.16%, rgb(36, 0, 255) 0deg, rgb(0, 135, 255) 67.5deg, rgb(108, 39, 157) 198.75deg, rgb(24, 38, 163) 251.25deg, rgb(54, 103, 196) 301.88deg, rgb(105, 30, 255) 360deg)",
-      "glow-lines": "linear-gradient(var(--direction),#9d9bf2 0.43%,#7877c6 14.11%,rgba(120,119,198,0) 62.95%)"
+      "glow-lines": "linear-gradient(var(--direction),#9d9bf2 0.43%,#7877c6 14.11%,rgba(120,119,198,0) 62.95%)",
+      "sunrise-gradient": 'radial-gradient(circle at bottom center,var(--color),transparent 70%);',
     },
     boxShadow: {
       primary: 'rgba(80, 63, 205, 0.5) 0px 1px 40px'
