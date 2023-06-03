a2c657a051ec93eb86223d1de477519b9ae21ce8
Nash <nashbach69@gmail.com>
2023-06-03 20:56:57 +0530

    rfactor the star gradients and add the bottom one

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,4 @@
-import classNames from 'classnames';
 import { Container } from './components/container';
-import { Stars } from './components/illustrations/stars';
 import { Clients } from './components/sections/clients';
 import { HomePageHero } from './components/sections/homepage-hero';
 import { IssueTracking } from './components/sections/issue-tracking';
@@ -8,6 +6,7 @@ import { MomentumCycles } from './components/sections/momentum-cycles';
 import { Roadmaps } from './components/sections/roadmaps';
 import { UnlikeAnyTool } from './components/sections/unlike-any-tools';
 import { Workflows } from './components/sections/workflows';
+import { StarComponent } from './components/stars';
 
 export default function Home() {
   return (
@@ -19,20 +18,13 @@ export default function Home() {
       <Container className='mt-[12.8rem] text-center md:mt-[25.6rem]'>
         <Clients />
       </Container>
-      <div
-        className={classNames(
-          'sunrise-mask bg-sunrise-background pointer-events-none relative z-[-1] mx-auto my-[-12.8rem] h-[60rem] overflow-hidden',
-          '[--color:#7877C6] before:absolute before:inset-0 before:h-full before:w-full before:bg-sunrise-gradient before:opacity-40',
-          'after:absolute after:-left-1/2 after:top-1/2 after:aspect-1/0.7 after:w-[200%] after:rounded-[50%] after:border-t after:border-[#7876c566] after:bg-background'
-        )}
-      >
-        <Stars />
-      </div>
+      <StarComponent />
       <UnlikeAnyTool />
       <IssueTracking />
       <MomentumCycles />
       <Roadmaps />
       <Workflows />
+      <StarComponent reverse={true} />
     </>
   );
 }
